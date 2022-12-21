94abded0cabef3a4fe82a6792d2e9ecd8161df98
nm <sharulnizar.razali@gmail.com>
2022-12-21 10:53:25 +0800

    basic uitableview

--- FreshXCodeProject/ViewController.swift
+++ FreshXCodeProject/ViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.myTableView.dataSource = self
+        self.myTableView.delegate = self
+        
         print("test 123...")
     }
 
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row \(indexPath.row) was selected!")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Hello World!"
+        return cell
+    }
+    
+}
